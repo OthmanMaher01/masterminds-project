e3977024f590331efcb457931ca60936565c8564
OthmanMaher <othmanmaher150@gmail.com>
2023-03-17 18:01:51 +0300

    changes

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -18,16 +18,16 @@
 
 
     <!-- Navbar Start -->
-    <div class="container-fluid absolute-top px-0 wow fadeIn" data-wow-delay="0.1s" :class="{'bg-white':!isAuthenticated}" style="postion:absolute">
+    <div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s" :class="{'bg-white':!isAuthenticated}" style="postion:absolute">
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
                 <h1 class="display-5 text-primary m-0">Tamweelk</h1>
             </a>
-            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <!-- <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <div class="navbar-nav ms-auto p-4 p-lg-0">
+            </button> -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+                <!-- <div class="navbar-nav ms-auto p-4 p-lg-0">
                     <a href="index.html" class="nav-item nav-link active">Home</a>
                     <a href="about.html" class="nav-item nav-link">About</a>
                     <a href="service.html" class="nav-item nav-link" v-if="isAuthenticated">Services</a>
@@ -46,7 +46,7 @@
                             <a href="404.html" class="dropdown-item">404 Page</a>
                         </div>
                     </div>
-                </div>
+                </div> -->
                 <div class="d-none d-lg-flex ms-2">
                     <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="">
                         <small class="fab fa-facebook-f text-primary"></small>
